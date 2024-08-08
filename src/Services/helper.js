5441f6355775c48b0ac0554bc65a78a71c5cecab
Sarfaraz <sarfarazkanganolli@gmail.com>
2024-08-08 16:30:41 +0530

    change inn url backend

--- src/Services/helper.js
+++ src/Services/helper.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://vi-sahay-backend.onrender.com";
+const BASE_URL = "https://vi-sahay-backend-1.onrender.com";
 // const BASE_URL = "http://localhost:5000";
 export default BASE_URL
